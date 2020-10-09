c3a167f9282296e15a6b802cb16412792f7ff8d9
Gergely M <gergely.meszaros@chameleon.sh>
2020-10-10 00:57:53 +0200

    ..df added as well as some new functions

--- immunity-bot.py
+++ immunity-bot.py
@@ -6,26 +6,42 @@ from PIL import ImageFilter
 from PIL import ImageEnhance
 client = discord.Client()
 
+def get_image_attachment_from_message(message):
+    return Image.open(BytesIO(requests.get(message.attachments[0].url).content))
+
+async def send_image_to_chat(img,channel):
+    img.save('./img.png')
+    file = discord.File("img.png", filename="img.png")
+    await channel.send("", file=file)
+
+def apply_contrast(contrast_amount,img):
+    return ImageEnhance.Contrast(img).enhance(contrast_amount)
+
+def saturate(saturation_amount,img):
+    return ImageEnhance.Color(img).enhance(saturation_amount)
+
+def apply_emboss(emboss_amount,img):
+    for i in range(emboss_amount):
+        img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    return img
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
-    
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('..enhance'):
-        if message.attachments:
-            response = requests.get(message.attachments[0].url)
-            img = Image.open(BytesIO(response.content))
-            
-            for i in range(8):
-                img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
-                
-            img.save('./temp.png')
-            file = discord.File("temp.png", filename="aaa.png")
-            await message.channel.send("enhancing..", file=file)
+    if message.attachments:
+        img = get_image_attachment_from_message(message);
+
+        if message.content.startswith('..enhance'):
+            img = apply_emboss(8,img);
+            await send_image_to_chat(img,message.channel);
+
+        if message.content.startswith('..df'):
+            img = apply_contrast(50,img)
+            await send_image_to_chat(img,message.channel);
 
 client.run('NzYzNTM3MTU0NzAxMzkzOTIw.X35JYw.t3Lr-aHa6ccnPDVbh59KL1BXkwM')
