9c956095fc544d56825e6fda56af858c4d61bcfc
cherry <gb.fouronetwo@gmail.com>
2022-11-15 23:36:44 +0100

    setup.bat added for windows

---
+++ setup.bat
@@ -0,0 +1,13 @@
+py -3 -m pip install pillow
+
+py -3 -m pip install requests
+
+py -3 -m pip install pyo
+
+py -3 -m pip install wand
+
+py -3 -m pip install -U discord.py
+
+py -3 -m pip install wxpython
+
+py -3 -m pip install numpy==1.19.3
