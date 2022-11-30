1a5e475973643ecd0fa9322f417de3e7be6a3d84
RadinQue <makramate12@gmail.com>
2022-11-30 19:00:13 +0100

    asd

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd immunity-bot
 ps -ef | grep "python3.8 ./immunity-bot.py" | grep -v grep | awk '{print $2}' | xargs kill
 git pull
-python3.8 ./immunity-bot/immunity-bot.py &
+python3.8 ./immunity-bot.py &
